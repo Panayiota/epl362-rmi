179073f0a2d086f3ea1dedab5eb1398810662a37
Panayiota Panayidou <peggyboom35@gmail.com>
2014-04-16 16:26:25 +0300

    Started Working on the client side GUI
    Gui contains only one button that test the connection with the server

--- epl362Project/src/epl362Project/client.java
+++ epl362Project/src/epl362Project/client.java
@@ -1,14 +1,55 @@
 package epl362Project;
 
+import javax.swing.*;
+
+import java.awt.Button;
+import java.awt.Dimension;
 import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class client {
-
+	static Communication comm;
 	public static void main(String[] args)throws Exception {
-		// TODO Auto-generated method stub
-String url=new String("rmi://localhost:2020/RMI");
-Communication comm=(Communication)Naming.lookup(url);
-System.out.println(comm.Communicate());
+		//create a url to locate the RMI server
+		String url=new String("rmi://localhost:2020/RMI");
+		//locate the rmi server
+		comm=(Communication)Naming.lookup(url);
+		
+		//start creating a gui
+		JFrame frame=new JFrame("");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Dimension d=new Dimension(100, 100);
+		frame.setSize(new Dimension(323, 326));
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
+		
+		JButton btnConnectionTest = new JButton("Connection Test");
+		btnConnectionTest.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					System.out.println(comm.Communicate());
+				} catch (RemoteException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		GridBagConstraints gbc_btnConnectionTest = new GridBagConstraints();
+		gbc_btnConnectionTest.gridx = 0;
+		gbc_btnConnectionTest.gridy = 0;
+		frame.getContentPane().add(btnConnectionTest, gbc_btnConnectionTest);
+		frame.setVisible(true);
 	}
+
 
 }
