21269e803f3b27907cc7fb699649a30e97ca9d6a
Panayiota Panayidou <peggyboom35@gmail.com>
2014-05-02 17:57:34 +0300

    created new exception helper class

---
+++ epl362Project/src/epl362Project/notImplementedException.java
@@ -0,0 +1,11 @@
+package epl362Project;
+
+/**
+ * Simple exception helper class for methods not yet implemented
+ */
+public class notImplementedException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public notImplementedException(){}
+}
